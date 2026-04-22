6c1249c578d697f28537f2fd4e169aec3022511d
mohamed mohamady <eng.mohamedmohamady94@gmail.com>
2026-04-22 19:09:49 +0200

    fix railway port

--- app.py
+++ app.py
@@ -70,5 +70,8 @@ def index():
     return render_template_string(INDEX_HTML)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
